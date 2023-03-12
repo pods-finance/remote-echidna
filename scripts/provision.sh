a8059c0cc3a2efaf8244a2f2f291ff2db95a32a3
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-11 23:28:59 -0300

    Fix script

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,11 +2,11 @@
 
 set -eux
 
-INSTANCE_ID="$1"
+S3_BUCKET="$1"
 PROJECT_GIT_CHECKOUT="$2"
-S3_BUCKET="$3"
+INSTANCE_ID="$3"
 
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "date: $DATE" > PROVISIONED.yml
-aws s3 mv PROVISIONED.yml s3://${S3_BUCKET}/${PROJECT_GIT_CHECKOUT}/${INSTANCE_ID}/status/
-aws s3 cp terraform.tfstate s3://${S3_BUCKET}/${PROJECT_GIT_CHECKOUT}/${INSTANCE_ID}/
+aws s3 mv PROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+aws s3 cp terraform.tfstate s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/
