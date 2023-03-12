f2ae8c886edd4bcbee8cecf411bcfb9adbd86f5f
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-12 01:51:10 -0300

    Update exit code for provision

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,4 +5,4 @@ set -eux
 S3_BUCKET="$1"
 PROJECT_GIT_CHECKOUT="$2"
 
-aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate .
+aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate . || true
