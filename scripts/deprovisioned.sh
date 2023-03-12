68c17cafe17100bb036ab0af845f81799267ecb9
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-12 02:41:33 -0300

    Create latest ref

--- scripts/deprovisioned.sh
+++ scripts/deprovisioned.sh
@@ -8,5 +8,6 @@ INSTANCE_ID="$3"
 
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "date: $DATE" > DEPROVISIONED.yml
-aws s3 mv DEPROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+aws s3 cp DEPROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+aws s3 cp DEPROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/latest/status/
 aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate .
--- scripts/provisioned.sh
+++ scripts/provisioned.sh
@@ -8,5 +8,6 @@ INSTANCE_ID="$3"
 
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "date: $DATE" > PROVISIONED.yml
-aws s3 mv PROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+aws s3 cp PROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+aws s3 cp PROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/latest/status/
 aws s3 cp terraform.tfstate s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/
