1d262bf2b7a8b8a1de320a8857737bb87a6558df
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-13 16:06:59 -0300

    Update deprovision

--- scripts/deprovision.sh
+++ scripts/deprovision.sh
@@ -6,7 +6,7 @@ S3_BUCKET="$1"
 
 for instance_id in $(aws s3 ls s3://$S3_BUCKET/4_FINISHED/ | awk '{print $NF}'); do
   echo $instance_id
-  aws s3 cp aws s3 ls s3://$S3_BUCKET/4_FINISHED/$instance_id .
+  aws s3 cp s3://$S3_BUCKET/4_FINISHED/$instance_id .
   DATE=$(grep 'date:' $instance_id); DATE=${DATE//*date: /}; 
   PROJECT_GIT_CHECKOUT=$(grep 'project_git_checkout:' $instance_id); PROJECT_GIT_CHECKOUT=${PROJECT_GIT_CHECKOUT//*project_git_checkout: /}; 
   INSTANCE_ID=$(grep 'instance_id:' $instance_id); INSTANCE_ID=${INSTANCE_ID//*instance_id: /}; 
