a56d093c3793e446aca1c24f81cf1b1a0044fb52
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-14 10:21:05 -0300

    Update indices to have PR updates

--- scripts/deprovision.sh
+++ scripts/deprovision.sh
@@ -4,19 +4,19 @@ set -eux
 
 S3_BUCKET="$1"
 
-for instance_id in $(aws s3 ls s3://$S3_BUCKET/4_FINISHED/ | awk '{print $NF}'); do
-  echo $instance_id
-  aws s3 cp s3://$S3_BUCKET/4_FINISHED/$instance_id .
-  DATE=$(grep 'date:' $instance_id); DATE=${DATE//*date: /}; 
-  PROJECT_GIT_CHECKOUT=$(grep 'project_git_checkout:' $instance_id); PROJECT_GIT_CHECKOUT=${PROJECT_GIT_CHECKOUT//*project_git_checkout: /}; 
-  INSTANCE_ID=$(grep 'instance_id:' $instance_id); INSTANCE_ID=${INSTANCE_ID//*instance_id: /}; 
+for instance_id_yml in $(aws s3 ls s3://$S3_BUCKET/4_FINISHED/ | awk '{print $NF}'); do
+  echo $instance_id_yml
+  aws s3 cp s3://$S3_BUCKET/4_FINISHED/$instance_id_yml .
+  DATE=$(grep 'date:' $instance_id_yml); DATE=${DATE//*date: /};
+  PROJECT_GIT_CHECKOUT=$(grep 'project_git_checkout:' $instance_id_yml); PROJECT_GIT_CHECKOUT=${PROJECT_GIT_CHECKOUT//*project_git_checkout: /};
+  INSTANCE_ID=$(grep 'instance_id:' $instance_id_yml); INSTANCE_ID=${INSTANCE_ID//*instance_id: /};
 
   NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
   DATEDIFF=$(( ($(date --date="$NOW" +%s) - $(date --date="$DATE" +%s) )/(60*60*24) ))
 
   if [ "$DATEDIFF" -gt 0 ]; then
-    aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate .
-    aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/vars.tfvars .
+    aws s3 cp s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/terraform.tfstate .
+    aws s3 cp s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/vars.tfvars .
 
     terraform init
     terraform destroy -var-file vars.tfvars -no-color -input=false -auto-approve
@@ -25,8 +25,8 @@ for instance_id in $(aws s3 ls s3://$S3_BUCKET/4_FINISHED/ | awk '{print $NF}');
     echo "project_git_checkout: $PROJECT_GIT_CHECKOUT" >> 5_DEPROVISIONED.yml
     echo "instance_id: $INSTANCE_ID" >> 5_DEPROVISIONED.yml
     
-    aws s3 cp 5_DEPROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
-    aws s3 cp 5_DEPROVISIONED.yml s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/latest/status/
+    aws s3 cp 5_DEPROVISIONED.yml s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/$INSTANCE_ID/status/
+    aws s3 cp 5_DEPROVISIONED.yml s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/latest/status/
     aws s3 rm s3://$S3_BUCKET/4_FINISHED/$INSTANCE_ID
   fi
 done;
---
+++ scripts/update-pr-comments.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -ux
+
+S3_BUCKET="$1"
+
+for status in 2_STARTED 3_RUNNING 4_FINISHED; do
+function get_pr_number() {
+  status=$1
+  for instance_id_yml in $(aws s3 ls s3://$S3_BUCKET/$status/ | awk '{print $NF}'); do
+    echo $instance_id_yml
+    aws s3 cp s3://$S3_BUCKET/$status/$instance_id_yml .
+    DATE=$(grep 'date:' $instance_id_yml); DATE=${DATE//*date: /}; 
+    INSTANCE_ID=$(grep 'instance_id:' $instance_id_yml); INSTANCE_ID=${INSTANCE_ID//*instance_id: /}; 
+    PR_NUMBER=$(grep 'pr_number:' $instance_id_yml); PR_NUMBER=${PR_NUMBER//*pr_number: /}; 
+
+    aws s3 rm s3://$S3_BUCKET/$status/$INSTANCE_ID
+    echo $PR_NUMBER
+    break;
+  done;
+}
+
+PR_NUMBER_2_STARTED="$(get_pr_number 2_STARTED)"
+echo "PR_NUMBER_2_STARTED=$PR_NUMBER_2_STARTED" >> $GITHUB_ENV
+
+PR_NUMBER_3_RUNNING="$(get_pr_number 3_RUNNING)"
+echo "PR_NUMBER_3_RUNNING=$PR_NUMBER_3_RUNNING" >> $GITHUB_ENV
+
+PR_NUMBER_4_FINISHED="$(get_pr_number 4_FINISHED)"
+echo "PR_NUMBER_4_FINISHED=$PR_NUMBER_4_FINISHED" >> $GITHUB_ENV
