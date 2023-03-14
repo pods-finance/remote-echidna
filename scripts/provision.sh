9494282652edafff147d84263c35cc3ef31e9e14
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-14 10:31:48 -0300

    Upload tfvars to S3 too

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,3 +6,4 @@ S3_BUCKET="$1"
 PROJECT_GIT_CHECKOUT="$2"
 
 aws s3 cp s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/terraform.tfstate . || true
+aws s3 cp s3://$S3_BUCKET/project_git_checkout/$PROJECT_GIT_CHECKOUT/vars.tfvars . || true
