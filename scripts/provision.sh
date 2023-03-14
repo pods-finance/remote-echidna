e3a3a6be4a3f163daa02801b60918fc3064f5365
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-13 23:03:31 -0300

    Try to add logs on PR

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,3 +6,9 @@ S3_BUCKET="$1"
 PROJECT_GIT_CHECKOUT="$2"
 
 aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate . || true
+curl \
+  -X POST \
+  $URL \
+  -H "Content-Type: application/json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  --data '{ "body": "remote-echidna: download terraform state" }'
