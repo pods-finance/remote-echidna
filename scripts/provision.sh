19c89f74cc1730df61170c05bc576eca3e409da7
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2023-03-13 23:38:10 -0300

    Rollback

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -4,13 +4,5 @@ set -eux
 
 S3_BUCKET="$1"
 PROJECT_GIT_CHECKOUT="$2"
-URL="$3"
-GITHUB_TOKEN="$4"
 
 aws s3 cp s3://$S3_BUCKET/$PROJECT_GIT_CHECKOUT/terraform.tfstate . || true
-curl \
-  -X POST \
-  $URL \
-  -H "Content-Type: application/json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  --data '{ "body": "remote-echidna: download terraform state" }'
